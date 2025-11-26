123c22255d5b917254b8747f404596a8f84985ff
iandev <iazinho321@gmail.com>
2025-11-26 13:44:23 -0300

    fiz o começo do front

---
+++ frontend/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo-home.css">
+    <title>home</title>
+</head>
+<body>
+    <header>
+        <img class="logo" src="assets/ChatGPT Image 26 de nov. de 2025, 13_29_56.png" alt="logo">
+        <nav>
+            <ul class="nav_links">
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Equipamentos</a></li>
+                <li><a href="#">Como pescar</a></li>
+            </ul>
+        </nav>
+        <a class="peixes"><button>Pescados</button></a>
+    </header>
+</body>
+</html>
