8e6ddb46f1877bb54a1a1fcbb0c94e44f441c42b
iandev <iazinho321@gmail.com>
2025-12-02 11:19:39 -0300

    quer equipamentos? então toma

--- frontend/equipamentos.php
+++ frontend/equipamentos.php
@@ -4,15 +4,41 @@ include_once "topo.php";
 
     <div class="parent">
         <div class="child">
-            <div class="comeco">1</div>
-            <div class="final">2</div>
+            <div class="container">
+                <div class="container">
+                <img class="ian4" src="assets/vara_de_pesca_deep_iii_2543113_1_26daafaefa0b4cdc0616d3faefe26781.webp">
+                </div>
+                <div class="container">
+                <h1 class="textou">A vara de pesca serve para lançar a isca a uma distância maior e controlar o movimento durante a pescaria. Ela ajuda o pescador a sentir as puxadas do peixe e facilita trazer o peixe até a margem ou barco.</h1>
+                </div>
+            </div>
         </div>
             
-        <div class="child"></div>
+        <div class="child">
+            <div class="container">
+                <div class="container2">
 
-        <div class="child"></div>
+                    <img class="ian5" src="assets/Jp_Cartela_De_Anzol_4330_Aco_Niquel_N6_Jau_Pesca.jpeg">
+                    <div class="container">
+                    <h1 class="textou2">O anzol é a peça que fisga o peixe. Ele segura a isca e, quando o peixe morde, sua ponta curva prende na boca, permitindo que o pescador recolha a captura com segurança.</h1>
+                </div>
+            </div>
+            </div>
+
+
+        </div>
+        <div class="child">
+            <div class="container">
+                <div class="container">
+                <img class="ian4" src="assets/shopping.webp">
+                </div><div class="container">
+                <h1 class="textou">A linha de pesca conecta o anzol à vara. Ela suporta o peso e a força do peixe, permitindo lançar e recolher a isca com precisão. Cada tipo de linha oferece resistência e flexibilidade diferentes.</h1>
+                </div>
+            </div>
+
+
+        </div>
         
-        <div class="child"></div>
         
     </div>
 </body>
